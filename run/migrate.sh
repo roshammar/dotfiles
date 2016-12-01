93ee6b1988f42d0f3d064977592a29942415b090
Kristoffer Röshammar <roshammar@gmail.com>
2016-12-01 19:57:34 +0100

    Add more items to migrate script

--- run/migrate.sh
+++ run/migrate.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/bash
 
 mkdir -p ~/migration/home
+mkdir ~/migration/home/.ipython
 
 cp -R ~/.ssh ~/migration/home
 cp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration
-cp ~/.z ~/.zhistory ~/migration
+cp ~/.z ~/.zhistory ~/migration/home
+cp -r ~/.ipython/logs/ ~/.ipython/profile_default/history.sqlite ~/migration/home/.ipython
+cp /etc/hosts ~/migration
