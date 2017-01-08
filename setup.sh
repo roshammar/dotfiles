ea786331f7580ee0579c5e529a38f5ce83e5736b
Kristoffer Röshammar <roshammar@gmail.com>
2017-01-08 23:41:30 +0100

    Fix for file names containing spaces

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Install apps, tools, dependencies
 install/install.sh
 
+# Fix for file names containing spaces
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 # Link config files
 pth=`pwd`/link/
 for fname in `ls -A $pth`
@@ -33,9 +37,12 @@ target_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 pth=`pwd`/sublime/
 for fname in `ls -A $pth`
 do
-  target_file="$target_dir$fname"
-  ln -s "$pth$fname" "$target_file"
+  target_file="${target_dir}${fname}"
+  ln -s "${pth}${fname}" "${target_file}"
 done
+
+# restore $IFS
+IFS=$SAVEIFS
 
 # Install Zsh
 zsh/install.sh
