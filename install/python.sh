e9a1d33f3d6b0c9bbef42edcc7f768543d3ba0f0
Kristoffer Röshammar <roshammar@gmail.com>
2017-03-13 14:57:32 +0100

    Rewrite installation script for Python packages

--- install/python.sh
+++ install/python.sh
@@ -1,64 +1,53 @@
 #!/usr/bin/env bash
 
-pips=(
-  pip
-  pip3
-)
+pip3 install -U pip
 
-packages=(
-  BeautifulSoup4
-  bson
-  chardet
-  click
-  cloudant
-  cython
-  cytoolz
-  fake-useragent
-  feather-format
-  feedparser
-  flask
-  flask_cors
-  fuzzywuzzy
-  gensim
-  google-cloud-datastore
-  hdbscan
-  jupyter
-  more-itertools
-  nltk
-  nose
-  pandas
-  pep257
-  pep8
-  pip-tools
-  pycrypto
-  pyflakes
-  pympler
-  pymysql
-  PyPDF2
-  python-dateutil
-  python-Levenshtein
-  pytz
-  regex
-  requests
-  rethinkdb
-  retry
-  scikit-learn
-  tabulate
-  textract
-  typing
-  ujson
-  unicodecsv
-  watchdog
-  wordcloud
-)
+pip3 install -U BeautifulSoup4
+pip3 install -U bson
+pip3 install -U chardet
+pip3 install -U click
+pip3 install -U cloudant
+pip3 install -U cython
+pip3 install -U cytoolz
+pip3 install -U fake-useragent
+pip3 install -U feather-format
+pip3 install -U feedparser
+pip3 install -U flask
+pip3 install -U flask_cors
+pip3 install -U fuzzywuzzy
+pip3 install -U gensim
+pip3 install -U google-cloud-datastore
+pip3 install -U hdbscan
+pip3 install -U jupyter
+pip3 install -U more-itertools
+pip3 install -U mypy
+pip3 install -U nltk
+pip3 install -U nose
+pip3 install -U pandas
+pip3 install -U pep257
+pip3 install -U pep8
+pip3 install -U pip-tools
+pip3 install -U pycrypto
+pip3 install -U pyflakes
+pip3 install -U pympler
+pip3 install -U pymysql
+pip3 install -U PyPDF2
+pip3 install -U python-dateutil
+pip3 install -U python-Levenshtein
+pip3 install -U pytz
+pip3 install -U regex
+pip3 install -U requests
+pip3 install -U rethinkdb
+pip3 install -U retry
+pip3 install -U scikit-learn
+pip3 install -U tabulate
+pip3 install -U textract
+pip3 install -U typing
+pip3 install -U ujson
+pip3 install -U unicodecsv
+pip3 install -U watchdog
+pip3 install -U wordcloud
 
-for pip in ${pips[@]}; do
-	$pip install --upgrade pip
-  $pip install "${packages[@]}"
-  ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future $pip install pgmagick
-  STATIC_DEPS=true $pip install lxml
-  $pip install cssselect
-done
-
-# Only available for Python 3
-pip3 install mypy
+ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future pip3 install pgmagick
+STATIC_DEPS=true pip3 install lxml
+pip3 install cssselect
