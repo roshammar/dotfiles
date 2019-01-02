b3c7a27a2d5c1096d8bc6a6f33f1f000bfb50015
Kristoffer Röshammar <roshammar@gmail.com>
2019-01-02 13:04:54 +0100

    Upgrade migrate script

--- run/migrate.sh
+++ run/migrate.sh
@@ -3,11 +3,19 @@
 mkdir -p ~/migration/home
 mkdir ~/migration/home/.ipython
 
+rm ~/.ssh/control/*
 cp -Rp ~/.ssh ~/migration/home
-cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration
-cp -Rp ~/.z ~/.zhistory ~/migration/home
-cp -Rp ~/.ipython/logs/ ~/.ipython/profile_default/history.sqlite ~/migration/home/.ipython
-cp /etc/hosts ~/migration
-cp ~/.local/share/fish/fish_history ~/migration
+cp -Rp ~/.ipython/logs ~/.ipython/profile_default/history.sqlite ~/migration/home/.ipython
+cp -Rp ~/.config/fish ~/migration/home
 
+mv ~/data ~/migration/home
+mv ~/work ~/migration/home
+mv ~/projects ~/migration/home
+
+cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration
+cp /etc/hosts ~/migration
+cp -p ~/.local/share/fish/fish_history ~/migration
 cp ~/Library/Application\ Support/Sublime\ Text\ 3/Local/License.sublime_license ~/migration
+
+DATE=`date +%Y-%m-%d`
+cp -Rp ~/migration /Volumes/Femman/migration-${DATE}
