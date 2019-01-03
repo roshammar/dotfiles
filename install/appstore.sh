d3e230172168b68d78ea5f2f897425b1fb92d64f
Kristoffer Röshammar <roshammar@gmail.com>
2019-01-03 17:35:22 +0100

    appstore.sh: add more apps to install

--- install/appstore.sh
+++ install/appstore.sh
@@ -3,3 +3,6 @@
 mas install 1091189122 # Bear
 mas install 1147976909 # Bredbandskollen
 mas install 568494494  # Pocket
+mas install 1035137927 # KeyKey
+mas install 441258766  # Magnet
+mas install 973134470  # Be Focused
