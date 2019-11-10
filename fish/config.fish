040bb9bc1b954b8410e5823dc48db6cedcad9a40
Kristoffer Röshammar <roshammar@gmail.com>
2019-11-10 22:49:11 +0100

    Export LANG and LC_ALL in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,9 @@ set -xg GOOGLE_CLOUD_PROJECT crawlica-1055
 set -x PATH /usr/local/opt/python/libexec/bin $PATH $HOME/bin
 set -x EDITOR nano
 
+set -x LC_ALL "en_US.UTF-8"
+set -x LANG "en_US"
+
 set -U __done_min_cmd_duration 60000
 
 function .    ; cd .. ; end
