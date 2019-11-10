ef6590ee09141ed70ebe4ffb8c551e60890f1471
Kristoffer Röshammar <roshammar@gmail.com>
2019-11-10 22:43:55 +0100

    No need to chsh to bash, we swith to fish later instead

--- install/install.sh
+++ install/install.sh
@@ -11,7 +11,6 @@ curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
 fi;
 
 ./appstore.sh
