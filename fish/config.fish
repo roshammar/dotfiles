c999188a27c82182c6c1df23200ab387d23e68e9
Kristoffer Röshammar <roshammar@gmail.com>
2019-11-27 14:05:37 +0100

    Add misc gnubin dirs to path in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -xg PYTHONPATH "$PYTHONPATH:$HOME/work/src/norum:$HOME/work/src:$HOME/projects"
 set -xg GOOGLE_CLOUD_PROJECT crawlica-1055
-set -x PATH /usr/local/opt/python/libexec/bin $PATH $HOME/bin
+set -x PATH /usr/local/opt/python/libexec/bin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/gnu-getopt/bin /usr/local/opt/coreutils/libexec/gnubin $PATH $HOME/bin
 set -x EDITOR nano
 
 set -x LC_ALL "en_US.UTF-8"
