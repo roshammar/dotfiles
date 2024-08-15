f38ee2f5a112a217ac0284e2fe24186fc99b5817
Kristoffer Röshammar <roshammar@gmail.com>
2024-08-15 11:37:07 +0200

    No need to install fish packages any more

--- fish/install.sh
+++ fish/install.sh
@@ -13,17 +13,8 @@ do
   ln -s $fname $functions_dir
 done
 
-# Install fisher package manager
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
 # Install shell
 if ! fgrep -q '/usr/local/bin/fish' /etc/shells; then
   echo '/usr/local/bin/fish' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/fish;
 fi;
-
-# Install some useful packages
-fisher add rafaelrinaldi/pure
-fisher add franciscolourenco/done
-fisher add jethrokuan/z
-
