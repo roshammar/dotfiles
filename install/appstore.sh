4255623971504cbf619d3289978e3fd492623a8a
Kristoffer Röshammar <roshammar@gmail.com>
2024-08-15 10:21:03 +0200

    Remove Magnet and Be Focused from appstore.sh

--- install/appstore.sh
+++ install/appstore.sh
@@ -4,8 +4,6 @@ mas install 1091189122 # Bear
 mas install 1147976909 # Bredbandskollen
 mas install 568494494  # Pocket
 mas install 1035137927 # KeyKey
-mas install 441258766  # Magnet
-mas install 973134470  # Be Focused
 mas install 881418622  # Affinity Publisher
 mas install 409201541  # Pages
 mas install 409203825  # Numbers
