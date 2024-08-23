debf7bf6a9c20ea8ffbb0f1e721f6f1d747157d3
Kristoffer Röshammar <roshammar@gmail.com>
2024-08-23 17:03:49 +0200

    Ask for password upfront in setup.py

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 # Prevent sleep while this script is running
 caffeinate -is &
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Link config files
 pth=`pwd`/link/
 for fname in `ls -A $pth`
